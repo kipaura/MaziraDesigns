fb810950166a132474bc9cf7d0cd9f834023a76b
kipaura <info@kipaura.com>
2025-04-22 00:55:18 -0500

    initial commit test branch

---
+++ lib/stripe/priceidhelper.ts
@@ -0,0 +1,8 @@
+import { priceIdLookup } from "./priceIdLookup";
+
+/**
+ * Returns the price ID for the given product name.
+ */
+export function getPriceId(productName: string): string {
+  return priceIdLookup[productName] || "";
+}
